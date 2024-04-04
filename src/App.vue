2df950cf6c4e5de5daf8aca0680ba2873f2cc93a
yeojisun <duwltjs5921@gmail.com>
2024-04-04 13:44:47 +0900

    메인 수정, 마커테스트이미지

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template> <naver-map
-    style="width: 100%; height: 400px"
+    style="width: 100%; height: 800px"
     :map-options="mapOptions"
   >
+   
     <naver-marker
-      latitude="37.51347"
-      longitude="127.041722"
-    />
+      v-for="(data) in datas"
+      :key="data.id"
+      :latitude="data.lat"
+      :longitude="data.lng"
+    >
+      <div class="marker">
+        <img :src="require(`@/assets/marker/${data.imageName}.png`)" width="70px" height="90px">
+      </div>
+    </naver-marker>
   </naver-map>
 </template>
 
@@ -14,16 +21,26 @@
 import { NaverMap } from 'vue3-naver-maps'
 import { NaverMarker } from 'vue3-naver-maps'
 
-const mapOptions = {
-  latitude: 37.51347, // 지도 중앙 위도
-  longitude: 127.041722, // 지도 중앙 경도
-  zoom: 13,
-}
 export default {
   name: 'App',
   components: {
     NaverMap,NaverMarker,
-  }
+  },
+  
+setup(){
+  const mapOptions = {
+  latitude: 37.51347, // 지도 중앙 위도
+  longitude: 127.041722, // 지도 중앙 경도
+  zoom: 13,
+}
+
+const datas = [{
+  id:'종로',lat: 37.51347,
+  lng: 127.041722,imageName:'marker-icon'
+}] // 마커를 그릴 데이터
+return {mapOptions, datas,}
+}
+
 }
 </script>
 
